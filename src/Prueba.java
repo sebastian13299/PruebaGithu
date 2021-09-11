7bb96361fb3d8173ed22d86629fb866206693df2
Sebastian Barahona Valverde <sebastian13299@hotmail.com>
2021-09-10 20:42:39 -0600

    agregacion de texto

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("Hola al compannero");
+        System.out.println("Hola al compannero numero 1");
     }
 }
